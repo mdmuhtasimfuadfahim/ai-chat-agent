19f34edfbc8d58a574810a26e291d64ab904d7c7
Md. Muhtasim Fuad Fahim <muhtasimfahim@iut-dhaka.edu>
2024-02-19 22:16:31 +0600

    welcome message added, updated prompt for better response

--- src/schemas/request/option.schema.js
+++ src/schemas/request/option.schema.js
@@ -3,6 +3,7 @@ import s from "fluent-json-schema";
 export default s.object()
     .prop("siteId", s.string()).required()
     .prop("instruction", s.string())
+    .prop("welcomeMgs", s.string())
     .prop("goal", s.string())
     .prop("conversationScopes", s.string())
     .prop("invalidQueryMgs", s.string())
